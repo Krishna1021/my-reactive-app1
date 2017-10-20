6c5e3ce25f9a2bf67253f7b93d0fde7e6119f089
Kr2f1w3 <Krishna_Udaram@gap.com>
2017-10-20 14:50:51 +0530

    file updated to trigger pipiline

--- src/main/java/com/example/myspringreactive/FluxFlexService.java
+++ src/main/java/com/example/myspringreactive/FluxFlexService.java
@@ -26,7 +26,7 @@ return reactor.core.publisher.Flux.zip(interaval,evnts).map(reactor.util.functio
 
     public String randomUser(){
 
-        String[] users="Chary, Ramana,Uday".split(",");
+        String[] users="Chary, Ramana,Ritu,Uday".split(",");
         return  users[new java.util.Random().nextInt(users.length)];
     }
 
